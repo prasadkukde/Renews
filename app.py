faf1ce247ae1a65381d7a2b9e1525d0223430c48
prasad <prasadkukde22@gmail.com>
2025-04-05 13:06:22 +0530

    Updated news scrapers and properties

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-import json
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, jsonify, request
+
+# News scrapers
 from backend.news.propertynews import scrape_property_news
 from backend.news.khaleejtimes import scrape_khaleej_news
 from backend.news.dailynewsegypt import scrape_dailyegypt_news
 from backend.news.googlenews import scrape_google_news
+
+# Property scrapers
 from backend.properties.competitornews import scrape_competitor_news
 from backend.properties.properties import scrape_properties
-
-from flask import Flask, render_template, jsonify, request
-
-# ... existing imports ...
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
@@ -19,40 +19,42 @@ def home():
 
 @app.route("/scrape", methods=["GET"])
 def index():
-    if request.method != "GET":
-        return "", 200
-
+    # UAE and Egypt news
     uae_articles = scrape_khaleej_news() + scrape_property_news()
     egypt_articles = scrape_dailyegypt_news()
 
+    # Regional news from Google News
     countries = ["saudi", "iran", "iraq", "kuwait", "qatar"]
     rest_articles = []
     for country in countries:
-        query = f"{country}"
-        rest_articles += scrape_google_news(query)
+        rest_articles += scrape_google_news(country)
 
-    aldar_properties = scrape_properties("https://www.bayut.com/companies/aldar-properties-104861/")
-    emaar_properties = scrape_properties("https://www.bayut.com/s/emaar-properties-sale-dubai/")
-    nakheel_properties = scrape_properties("https://www.bayut.com/s/nakheel-properties-sale-dubai/")
-    damac_properties = scrape_properties("https://www.bayut.com/s/damac-properties-sale-dubai/")
+    # Helper to scrape a competitor's data
+    def get_competitor_data(name, url, logos):
+        return {
+            "name": name,
+            "news": scrape_competitor_news(name.lower().replace(" ", "-")),
+            "properties": scrape_properties(url),
+            "logos": logos
+        }
 
-    aldar_news = scrape_competitor_news("aldar-properties")
-    emaar_news = scrape_competitor_news("emaar-properties")
-    damac_news = scrape_competitor_news("damac-properties")
-    nakheel_news = scrape_competitor_news("nakheel")
-
+    # Competitor sections
     competitors = [
-        {"name": "Aldar", "news": aldar_news, "properties": aldar_properties, "logos": ["aldar.jpg", "aldar2.jpg"]},
-        {"name": "Emaar", "news": emaar_news, "properties": emaar_properties, "logos": ["emaar1.jpg", "emaar2.jpg"]},
-        {"name": "DAMAC", "news": damac_news, "properties": damac_properties, "logos": ["damac1.jpg", "damac2.jpg"]},
-        {"name": "Nakheel", "news": nakheel_news, "properties": nakheel_properties, "logos": ["nakheel1.png", "nakheel2.jpg"]}
+        get_competitor_data("Aldar", "https://www.bayut.com/companies/aldar-properties-104861/", ["aldar.jpg", "aldar2.jpg"]),
+        get_competitor_data("Emaar", "https://www.bayut.com/s/emaar-properties-sale-dubai/", ["emaar1.jpg", "emaar2.jpg"]),
+        get_competitor_data("DAMAC", "https://www.bayut.com/s/damac-properties-sale-dubai/", ["damac1.jpg", "damac2.jpg"]),
+        get_competitor_data("Nakheel", "https://www.bayut.com/s/nakheel-properties-sale-dubai/", ["nakheel1.png", "nakheel2.jpg"]),
     ]
 
-    return render_template("index.html", 
-                           country1_articles=uae_articles, 
-                           country2_articles=egypt_articles, 
-                           country3_articles=rest_articles, 
-                           competitors=competitors)
+    return render_template(
+        "index.html",
+        country1_articles=uae_articles,
+        country2_articles=egypt_articles,
+        country3_articles=rest_articles,
+        competitors=competitors
+    )
 
-
-
+# Required for Render or any cloud service to detect and bind the correct port
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
